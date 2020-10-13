2ced2c53418c843c0798925233c970dda604fea2
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-10-12 19:24:39 -0500

    finished wow

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
+import {fetchShow} from './api/fetchShow';
 
 import { formatSeasons } from "./utils/formatSeasons";
 
@@ -14,17 +15,13 @@ export default function App() {
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
-  useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
+  useEffect( () => {
+
+    fetchShow().then(res=>{
+      console.log(res)
+      setShow(res);
+      setSeasons(formatSeasons(res.data._embedded.episodes));
+    })
     fetchShow();
   }, []);
 
@@ -38,9 +35,9 @@ export default function App() {
 
   return (
     <div className="App">
-      <img className="poster-img" src={show.image.original} alt={show.name} />
+      <img className="poster-img" src={show.data.image.original} alt={show.name} />
       <h1>{show.name}</h1>
-      {parse(show.summary)}
+      {parse(show.data.summary)}
       <Dropdown
         options={Object.keys(seasons)}
         onChange={handleSelect}
